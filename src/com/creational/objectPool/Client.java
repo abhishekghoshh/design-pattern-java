86bbc013ab152aa391d051393e7e56604161613b
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-09-27 12:16:44 +0530

    ObjectPool added

--- src/com/creational/objectPool/Client.java
+++ src/com/creational/objectPool/Client.java
@@ -1,5 +1,44 @@
 package com.creational.objectPool;
 
-public class Client {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
+import com.creational.objectPool.jdbc.Connection;
+import com.creational.objectPool.jdbc.JDBCConnection;
+import com.creational.objectPool.pool.ObjectPool;
+
+public class Client {
+	private static AtomicLong processNo=new AtomicLong(0);
+	private final static int count =10;
+	
+	public static void main(String ags[]) {
+		
+		ObjectPool<Connection> pool = new ObjectPool<Connection>(4, 10, 5) {
+			protected Connection createObject(){
+				return new JDBCConnection(processNo.incrementAndGet());
+			}
+		};
+		
+		pool.shutdown();
+		
+		ExecutorService executor = Executors.newFixedThreadPool(8);
+    	
+    	List<Process> processList = new ArrayList<>(count);
+    	for(int i=0;i<count;i++) {
+    		processList.add(new Process(pool, i));
+    	}
+    	for(int i=0;i<count;i++) {
+    		executor.execute(processList.get(i));
+    	}
+        executor.shutdown();
+        try {
+            executor.awaitTermination(30, TimeUnit.SECONDS);
+        } catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+	}
 }
---
+++ src/com/creational/objectPool/Process.java
@@ -0,0 +1,23 @@
+package com.creational.objectPool;
+
+import com.creational.objectPool.jdbc.Connection;
+import com.creational.objectPool.pool.ObjectPool;
+
+public class Process implements Runnable{
+	private ObjectPool<Connection> pool;
+	private int threadNo;
+
+	public Process(ObjectPool<Connection> pool, int threadNo) {
+		this.pool = pool;
+		this.threadNo = threadNo;
+	}
+	public void run() {
+		Connection connection = pool.borrowObject();
+		System.out.println("Thread " + threadNo + ": Object with process no. " + connection.getProcessNo() + " was borrowed");
+		
+		connection.connect();
+		
+		pool.returnObject(connection);
+		System.out.println("Thread " + threadNo +": Object with process no. " + connection.getProcessNo() + " was returned");
+	}
+}
