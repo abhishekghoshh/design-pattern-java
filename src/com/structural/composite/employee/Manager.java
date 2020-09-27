e3134694ee472b4b1d43ed71083cdaede73c6df0
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-09-27 23:53:12 +0530

    commit

---
+++ src/com/structural/composite/employee/Manager.java
@@ -0,0 +1,47 @@
+package com.structural.composite.employee;
+
+public class Manager implements Employee {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(Employee employee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(Employee employee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Employee getChild(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
